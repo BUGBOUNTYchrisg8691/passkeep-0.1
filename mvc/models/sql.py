e3952833cdea8766c4c6a039f7fea61c07437cb8
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-08-14 00:09:07 -0700

    more MVC update 8/14/20 03:08

--- mvc/models/sql.py
+++ mvc/models/sql.py
@@ -5,6 +5,7 @@ import pymysql
 import sys
 import time
 
+
 class DatabaseInteraction:
     def __init__(self):
         self.DB_CREDS = {
@@ -48,20 +49,22 @@ class DatabaseInteraction:
     
         try:
             print('Adding new user...')
-            self.cursor.execute(users_statement, (first_name, last_name, email))
+            self.cursor.execute(users_statement,
+                                (first_name, last_name, email))
             self.conn.commit()
-            self.cursor.execute('''SELECT id FROM users WHERE email=%s''', email)
+            self.cursor.execute('''SELECT id FROM users WHERE email = %s''',
+                                email)
             uid = self.cursor.fetchone()[0]
-            self.cursor.execute(user_creds_statement, (int(uid), master_password,
-                                                  master_salt))
+            self.cursor.execute(user_creds_statement,
+                                (int(uid), master_password, master_salt))
             self.conn.commit()
         except Exception as e:
             sys.exit('An exception has occurred: ' + str(e))
         else:
             print('New user successfully added.')
 
-    def add_entry(self, email, user_id, service, username, hashed_password, salt,
-                  **entry_info):
+    def add_entry(self, email, user_id, service, username, hashed_password,
+                  salt, **entry_info):
         entries_statement = '''INSERT INTO entries(
         user_id, service, username, password)
         VALUES(%s, %s, %s, %s)'''
@@ -89,7 +92,7 @@ class DatabaseInteraction:
                                                 hashed_password))
             self.conn.commit()
             self.cursor.execute('''SELECT entry_id FROM entries
-            WHERE user_id=%s AND service=%s''', (int(user_id), service))
+            WHERE user_id = %s AND service = %s''', (int(user_id), service))
             entry_id = self.cursor.fetchone()[0]
             self.cursor.execute(salts_statement, (int(entry_id), salt))
             self.conn.commit()
@@ -97,6 +100,94 @@ class DatabaseInteraction:
             sys.exit('An exception has occurred: ' + str(e))
         else:
             print('Entry successfully added.')
+
+    def delete_entry(self, entry_id):
+        entry_statement = '''DELETE FROM entries WHERE entry_id = %s'''
+        salt_statement = '''DELETE FROM salts WHERE entry_id = %s'''
+
+        if self.conn.open:
+            try:
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                sys.exit(f'An Exception occurred: ' + str(e))
+        else:
+            try:
+                print('Reconnecting...')
+                self.conn = pymysql.Connect(**self.DB_CREDS)
+            except Exception as e:
+                sys.exit('An Exception occurred: ' + str(e))
+            else:
+                print('Connected.')
+        try:
+            print(f'Deleting entry...')
+            self.cursor.execute(entry_statement, (int(entry_id)))
+            self.conn.commit()
+        except Exception as e:
+            sys.exit('An exception occurred: ' + str(e))
+        else:
+            print('Entry successfully deleted.')
+
+    def edit_entry(self, option, entry_id, new_item, **new_entry_info):
+        if option.lower() == 'username':
+            sql_statement = '''UPDATE entries SET username = %s
+            WHERE entry_id = %s'''
+        elif option.lower() == 'password':
+            sql_statement = '''UPDATE entries SET password = %s
+            WHERE entry_id = %s'''
+        else:
+            sys.exit('An error occurred.')
+
+        if self.conn.open:
+            try:
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                sys.exit('An exception occurred: ' + str(e))
+        else:
+            try:
+                print('Reconnecting...')
+                self.conn = pymysql.Connect(**self.DB_CREDS)
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                sys.exit('An exception ovvurred: ' + str(e))
+            else:
+                print('Connected.')
+
+        try:
+            self.cursor.execute(sql_statement, (new_item, int(entry_id)))
+            self.conn.commit()
+        except Exception as e:
+            sys.exit('An exception occurred: ' + str(e))
+        else:
+            print('Entry successfully updated.')
+
+
+    def delete_user(self, user_id):
+        sql_statement = '''DELETE FROM users WHERE user_id = %s'''
+
+        if self.conn.open:
+            try:
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                sys.exit('An exception occurred: ' + str(e))
+        else:
+            try:
+                print('Reconnecting...')
+                self.conn = pymysql.Connect(**self.DB_CREDS)
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                sys.exit('An exception has occurred: ' + str(e))
+            else:
+                print('Connected.')
+
+        try:
+            print('Deleting account...')
+            self.cursor.execute(sql_statement, (user_id))
+            self.conn.commit()
+        except Exception as e:
+            sys.exit('An exception occurred: ' + str(e))
+        else:
+            print('Account successfuly deleted.')
+
 
 if __name__ == '__main__':
     #  new_user = {
@@ -107,16 +198,23 @@ if __name__ == '__main__':
         #  'master_salt': 'testsalt'
     #  }
 
-    user_entry = {
-        'email': 'chrisg@yahoo.com',
-        'user_id': 7,
-        'service': 'google',
-        'username': 'chrisg@yahoo.com',
-        'hashed_password': 'gAAAAABfNg6JT4l07Qg9DKGhSF5OmCd5irqv-5JW_yU-74dNs4XdFMqFK8vk7d5SMCe_H0Z02zQZrpH18-WcVkj6vI6JOfxJKNFMGhG_DKH4sMTY5MMFxPk=',
-        'salt': 'testsalt'
-        
+    #  user_entry = {
+        #  'email': 'chrisg@yahoo.com',
+        #  'user_id': 7,
+        #  'service': 'google',
+        #  'username': 'chrisg@yahoo.com',
+        #  'hashed_password': 'gAAAAABfNg6JT4l07Qg9DKGhSF5OmCd5irqv-5JW_yU-74dNs4XdFMqFK8vk7d5SMCe_H0Z02zQZrpH18-WcVkj6vI6JOfxJKNFMGhG_DKH4sMTY5MMFxPk=',
+        #  'salt': 'testsalt'
+#
+    #  }
+
+    entry_info = {
+        'option': 'password',
+        'entry_id': 18, 
+        'new_item': 'gAAAAABfNjgB2mMvxQiEiHRHxGxa9bhgbrJitqiTZOUzpWsrinj5iVOLkBFe8Hf95gBrfPx3hpbmfrhGlo_tSKwtaIfVIh-aJQ=='
     }
 
     dbi = DatabaseInteraction()
     #  dbi.add_user(**new_user)
-    dbi.add_entry(**user_entry)
+    #  dbi.add_entry(**user_entry)
+    dbi.edit_entry(**entry_info)
